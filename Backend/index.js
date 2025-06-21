a94011373f58280ddea4af7f7514703d148d77d7
Aryan Sharma <aryansharma06127@gmail.com>
2025-06-21 18:37:02 +0530

    msda

--- Backend/index.js
+++
@@ -1,9 +0,0 @@
-
-import express from "express"
-
-import authRoutes from "./src/routes/auth.routes.js";
-const app = express();
-app.use("api/auth",authRoutes)
- app.listen(5173,()=>{
-    console.log("server is running on port 5173");
-  } )
