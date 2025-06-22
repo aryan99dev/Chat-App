aeacea5fa9cee3f0a25754eb27d014b986d0af7b
Aryan Sharma <aryansharma06127@gmail.com>
2025-06-22 11:39:27 +0530

    node modules update

--- Frontend/src/App.jsx
+++ Frontend/src/App.jsx
@@ -2,6 +2,10 @@ import React from 'react'
 import Navbar from './components/Navbar';
 import { Routes , Route } from 'react-router-dom';
 import HomePage from './pages/HomePage';
+import SettingsPage from './pages/SettingsPage';
+import ProfilePage from './pages/ProfilePage';
+import SignUpPage from './pages/SignUpPage';
+import LoginPage from './pages/LoginPage';
 
 
 const App = () => {
@@ -10,6 +14,10 @@ const App = () => {
 <Navbar />
 <Routes>
   <Route path='/' element={<HomePage/>}></Route>
+  <Route path='/' element={<SignUpPage/>}></Route>
+  <Route path='/' element={<LoginPage/>}></Route>
+  <Route path='/' element={<SettingsPage/>}></Route>
+  <Route path='/' element={<ProfilePage/>}></Route>
 </Routes>
 </>
   )
