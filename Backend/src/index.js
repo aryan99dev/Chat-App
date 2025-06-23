677db5dc49cf97ae287da7b01fa7bb1c6c5f6d14
k5hitiz <kshitizsharma0001.com>
2025-06-23 11:11:52 +0530

    khedi peeyush

--- Backend/src/index.js
+++ Backend/src/index.js
@@ -3,9 +3,11 @@ import express from "express";
 
 import dotenv from "dotenv";
 
+
 import authRoutes from "./routes/auth.routes.js";
 
-import cors from "cors";
+
+import { connectDB } from "./lib/db.js";
 
 
 const app = express();
@@ -17,16 +19,12 @@ dotenv.config();
 
 const PORT =process.env.PORT
 
-app.use(express.json());
-app.use(cookieParser());
-app.use(cors ({
-  origin: "http://localhost:5173",
-  credentials: true
-}))
+
 
 app.use("/api/auth", authRoutes)
 
 
  app.listen(PORT, ()=>{
     console.log("server is running on PORT:" + PORT);
+    connectDB();
   } )
